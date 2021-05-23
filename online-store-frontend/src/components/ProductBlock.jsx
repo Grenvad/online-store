5b47b616bbe71101a48c3d347b84d9e397d0448d
Tetsu <servivce.icelemon@gmail.com>
2021-05-24 00:32:18 +0300

    Added new app style

--- online-store-frontend/src/components/ProductBlock.jsx
+++ online-store-frontend/src/components/ProductBlock.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Button from "./Button";
-import { ReactComponent as PlusIcon } from "../assets/img/plus.svg";
 
 function ProductBlock({ id, name, description, imageUrl, onClickAdd, price }) {
   const onAddItem = () => {
@@ -21,10 +20,14 @@ function ProductBlock({ id, name, description, imageUrl, onClickAdd, price }) {
       <div className="item-block__bottom">
         <div className="item-block__price">{price} р.</div>
         <Button onClick={onAddItem} className="button--add" outline>
-          <PlusIcon />
-          <span>в корзину</span>
+          <span>В корзину</span>
         </Button>
       </div>
+
+      <div className="border-top-left"></div>
+      <div className="border-bottom-right"></div>
+      <div className="border-bottom-left"></div>
+      <div className="border-top-right"></div>
     </div>
   );
 }
